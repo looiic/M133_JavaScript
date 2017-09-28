f200f9520bd91bd22365a724ea1469f1902623e7
looiic <loic.jermann@gibmit.ch>
2017-09-28 11:14:56 +0200

    Changes
    Some changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,69 @@
 $(function(){
 
+  var selectedWeek;
+  getTodayWeek();
   getBerufe();
 
   $('#beruf').on('change', getKlassen);
   $('#klasse').on('change', getTable);
+  $('#plus').on('click', plusWeek);
+  $('#minus').on('click', minusWeek);
 
+
+  function plusWeek(){
+    var selectedWeek = $('#weekButton').html();
+    var weeks = selectedWeek.split('-')[0];
+    if(weeks == '52'){
+      var year = parseInt(selectedWeek.split('-')[1]) + 1;
+      selectedWeek = '1-' + year;
+    }else{
+      weeks = parseInt(weeks) + 1;
+      selectedWeek = weeks + '-' + selectedWeek.split('-')[1];
+    }
+    $('#weekButton').html(selectedWeek);
+    getTable();
+  }
+
+  function minusWeek(){
+    var selectedWeek = $('#weekButton').html();
+    var weeks = selectedWeek.split('-')[0];
+    if(weeks == '1'){
+      var year = parseInt(selectedWeek.split('-')[1]) - 1;
+      selectedWeek = '52-' + year;
+    }else{
+      weeks = parseInt(weeks) - 1;
+      selectedWeek = weeks + '-' + selectedWeek.split('-')[1];
+    }
+    $('#weekButton').html(selectedWeek);
+    getTable();
+  }
+
+  function getTodayWeek(){
+    var today = new Date();
+
+    var thisYear = today.getFullYear();
+    var time1970 = today.getTime();
+
+    var milisecInYear = today.getTime() - ( ( today.getFullYear() - 1970 ) * 31536000000 );//31536000000 is one Year
+    milisecInYear = milisecInYear - (( today.getDay() + 1 ) * 86400000) //86400000 is one Day
+    var week = milisecInYear / 604800000;//604800000 is one Week
+    week = Math.round(week)-1;
+    week = week + '-' + today.getFullYear();
+    $('#weekButton').html(week);
+  }
 
   function getTable(){
     var selectedKlasse = $('#klasse').val();
-    $.getJSON('http://home.gibm.ch/interfaces/133/tafel.php?klasse_id=' + selectedKlasse, function(result){
+    var thisWeek = $('#weekButton').html();
+    $.getJSON('http://home.gibm.ch/interfaces/133/tafel.php?klasse_id=' + selectedKlasse + '&woche=' + thisWeek, function(result){
           $('#divTable').show();
           $('#table').find("tr:gt(0)").remove();
+          console.log(result);
           $.each(result, function(i, field){
-            //hier weiterschaffen
-            var tr = '<tr><td>' + field.tafel_datum + '</td><td>' + field.tafel_wochentag + '</tr>'
-            $('#table').append();
+            console.log(field);
+            var tr = '<tr><td>' + field.tafel_datum + '</td><td>' + field.tafel_wochentag + '</td><td>' + field.tafel_von + '</td><td>' + field.tafel_bis
+             + '</td><td>' + field.tafel_lehrer + '</td><td>' + field.tafel_fach + '</td><td>' + field.tafel_raum + '</td></tr>';
+            $('#table').append(tr);
           });
     }).fail(function(jqXHR, textStatus, errorThrown) {
       alert('Fehler');
