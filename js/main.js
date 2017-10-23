a50423cb0b60952cba9e1c8b4e01bff205a5366c
looiic <loic.jermann@gibmit.ch>
2017-10-23 10:52:09 +0200

    Added one comment

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ $(function(){
             });
             var beruf = getCookie('beruf');
             if (beruf != null){
-              //Cookies sollen nur gelesen werden, wenn Seite neu geladen wurde und nicht wenn man einfach den Beruf ändert
+              //Cookies sollen nur gelesen werden, wenn Seite neu geladen wurde und nicht wenn man einfach den Beruf ändert 
               readCookies = true;
               $('#beruf').val(beruf);
               $('#divKlasse').show();
